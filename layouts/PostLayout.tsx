78ced29abbf19c4f6e7a6ae91b9f3f12388e8d54
Raffael Rott <raffaelrott@protonmail.com>
2023-08-31 22:19:45 +0200

    # Adapted Theme - removed Logo from Mainpage - Changed Blog Layout - Removed Tailwind Nextjs Theme link - Removed Tags link

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -5,7 +5,6 @@ import Comments from '@/components/Comments'
 import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
 import SectionContainer from '@/components/SectionContainer'
-import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
@@ -39,8 +38,11 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
+            <div className="space-y-1 text-left">
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+              <dl className="space-y-10 pt-10 text-left">
                 <div>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
@@ -50,47 +52,50 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   </dd>
                 </div>
               </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
+              {authorDetails.map((author, key) => (
+                <dd className="text-gray-500 dark:text-gray-400" key={key}>
+                  {author.name}
+                </dd>
+              ))}
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width={38}
-                          height={38}
-                          alt="avatar"
-                          className="h-10 w-10 rounded-full"
-                        />
-                      )}
-                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
+            <div />
+            {/*<dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+                            <dt className="sr-only">Authors</dt>
+                            <dd>
+                                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                                    {authorDetails.map((author) => (
+                                        <li className="flex items-center space-x-2" key={author.name}>
+                                            {author.avatar && (
+                                                <Image
+                                                    src={author.avatar}
+                                                    width={60}
+                                                    height={60}
+                                                    alt="avatar"
+                                                    className="h-30 w-30 rounded-full"
+                                                />
+                                            )}
+                                            <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                                                <dt className="sr-only">Name</dt>
+                                                <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
+                                                <dt className="sr-only">Twitter</dt>
+                                                <dd>
+                                                    {author.twitter && (
+                                                        <Link
+                                                            href={author.twitter}
+                                                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                                                        >
+                                                            {author.twitter.replace('https://twitter.com/', '@')}
+                                                        </Link>
+                                                    )}
+                                                </dd>
+                                            </dl>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </dd>
+                        </dl>*/}
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
